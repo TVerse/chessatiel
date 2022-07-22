32ed740ed6fa2940ac83627d9b2aaab29502216e
Tim Evers <29369097+TVerse@users.noreply.github.com>
2022-07-22 20:40:19 +0200

    Reenable benchmarking

--- crates/guts/benches/perft.rs
+++ crates/guts/benches/perft.rs
@@ -1,17 +1,22 @@
-// use criterion::{black_box, criterion_group, criterion_main, Criterion};
-// use guts::MoveGenerator;
-// use guts::Position;
-// use std::str::FromStr;
-//
-// fn bench_perft_kiwipete_4(c: &mut Criterion) {
-//     let movegen = MoveGenerator::new();
-//     let pos =
-//         Position::from_str("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")
-//             .unwrap();
-//     c.bench_function("perft kiwipete 4", |b| {
-//         b.iter(|| movegen.perft(black_box(&pos), black_box(4)))
-//     });
-// }
-//
-// criterion_group!(name = perft, targets = bench_perft_kiwipete_4);
-// criterion_main!(perft);
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use guts::MoveGenerator;
+use guts::Position;
+use std::str::FromStr;
+use std::time::Duration;
+
+fn perft_kiwipete_4(c: &mut Criterion) {
+    let movegen = MoveGenerator::new();
+    let pos =
+        Position::from_str("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")
+            .unwrap();
+    c.bench_function("perft kiwipete 4", |b| {
+        b.iter(|| movegen.perft(black_box(&pos), black_box(4)))
+    });
+}
+
+criterion_group! {
+    name = perft;
+    config = Criterion::default().measurement_time(Duration::from_secs(30));
+    targets = perft_kiwipete_4
+}
+criterion_main!(perft);
